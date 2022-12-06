41d368a47b3127ff025324f1eeeb47dfb28a2ad6
slivcaigor <slivcaigor@gmail.com>
2022-12-06 14:44:35 +0100

    Initial files

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/*
+Descrizione:
+Attraverso l’apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email
+e stamparli in pagina all’interno di una lista.
+(chiaramente usando Vue, etc.,)
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+*/
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+      message: 'Hello Vue!'
+    }
+  }
+}).mount('#app')
+
