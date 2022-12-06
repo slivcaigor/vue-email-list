97e3bf9719d254a95deef3629f8937cfc7fde9b4
slivcaigor <slivcaigor@gmail.com>
2022-12-06 16:10:43 +0100

    Email generated

--- js/main.js
+++ js/main.js
@@ -13,8 +13,20 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      emailList: [],
     }
+  },
+  methods: {
+    // The get method called by the function
+    onGet() {
+      for (let i = 0; i < 10; i++) {
+        axios
+          .get("https://flynn.boolean.careers/exercises/api/random/mail")
+          .then((response) => {
+            this.emailList.push(response.data.response);
+          })
+      }
+    },
   }
 }).mount('#app')
 
